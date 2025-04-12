6c11aec5cd61add53732fe42d4e2ffb2ad73ce1d
Shalini <shaay208@mymail.unisa.edu.au>
2025-04-12 21:00:51 +0530

    first commit

---
+++ Assignment 1.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment 1.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment 1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
