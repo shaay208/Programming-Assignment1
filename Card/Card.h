deffa9d8ea6f59e161915669e9a898d0d734fd29
Shalini <shaay208@mymail.unisa.edu.au>
2025-04-16 02:41:06 +0930

    Added comments for Card.h file

--- Card/Card.h
+++ Card/Card.h
@@ -2,20 +2,27 @@
 #include <string>
 #include "CardType.h"
 
+// Forward declarations
 class Game;
 class Player;
 
 class Card {
 protected:
-    CardType type;
-    int value;
+    CardType type;  // Type of the card
+    int value;      // Value of the card
 
 public:
+    // Constructor
     Card(CardType type, int value);
+
+    // Destructor
     virtual ~Card() = default;
 
+    // Getters
     CardType getType() const;
     int getValue() const;
+
+    // methods to be implemented in the card classes
     virtual int getScoreValue() const;
     virtual std::string str() const = 0;
     virtual void play(Game& game, Player& player) = 0;
