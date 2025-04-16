0a00af6b4868004f168efc5a330eed78bd49c8dc
Shalini <shaay208@mymail.unisa.edu.au>
2025-04-17 00:20:36 +0930

    Add play logic and bust check to Card class
    - Implement play method to execute ability if player has not busted.
    - Add wouldCauseBust method to detect duplicate card types in play area.
    - Mark willAddToBank method as const.
    - Include <algorithm> for std::any_of usage.

--- Card/Card.cpp
+++ Card/Card.cpp
@@ -2,7 +2,7 @@
 #include "../Game/Game.h"
 #include "../Player/Player.h"
 #include <iostream>
-#include <string>
+#include <algorithm>
 
 // Constructor: Initializes the card with a specific type and value
 Card::Card(CardType type, int value) : type(type), value(value) {}
@@ -22,8 +22,25 @@ int Card::getScoreValue() const {
     return value;
 }
 
+// Executes the card's ability, but only if the player has not busted
+void Card::play(Game& game, Player& player) {
+    // Execute the card's ability only if the player hasn't busted
+    if (!player.hasBusted()) {
+        executeAbility(game, player);
+    }
+}
+
 // A placeholder method called when the card is about to be added to a player's bank
 // This can be overridden in derived classes to define special behavior
-void Card::willAddToBank(Game& game, Player& player) {
-    // Currently no special behavior defined
+void Card::willAddToBank(Game& game, Player& player) const {
+    // Default implementation does nothing
+}
+
+// Checks if playing this card would cause the player to bust, based on cards already in play
+bool Card::wouldCauseBust(const std::vector<CardPtr>& playArea) const {
+    // Check if there's already a card of the same type in the play area
+    return std::any_of(playArea.begin(), playArea.end(),
+        [this](const CardPtr& card) {
+            return card && card->getType() == this->type;
+        });
 }
