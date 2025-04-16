26de28ed3122a35a6b233383493da256131e8c35
Shalini <shaay208@mymail.unisa.edu.au>
2025-04-17 00:00:58 +0930

    Refactor Card class and add smart pointers
    - Replace raw pointers with smart pointers for better memory management.
    - Define `CardType` enum for card types.
    - Make `type` and `value` constants.
    - Rename methods to better describe card abilities.
    - Add `wouldCauseBust` and `canPlayOn` methods.
    - Mark methods as `const` where applicable.

--- Card/Card.h
+++ Card/Card.h
@@ -1,30 +1,63 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <memory>
 #include "CardType.h"
 
 // Forward declarations
 class Game;
 class Player;
+class Card;
+
+// Define the smart pointer type alias before using it
+using CardPtr = std::shared_ptr<Card>;
+using CardCollection = std::vector<CardPtr>;
+
+enum class CardType {
+    CANNON,
+    CHEST,
+    KEY,
+    SWORD,
+    HOOK,
+    ORACLE,
+    MAP,
+    MERMAID,
+    KRAKEN
+};
 
 class Card {
 protected:
-    CardType type;  // Type of the card
-    int value;      // Value of the card
+    // Type of the card (made const as it shouldn't change)
+    const CardType type; 
+    // Value of the card (made const as it shouldn't change)
+    const int value;      
 
 public:
     // Constructor
     Card(CardType type, int value);
 
-    // Destructor
+    // Virtual destructor for proper cleanup of derived classes
     virtual ~Card() = default;
 
-    // Getters
-    CardType getType() const;
-    int getValue() const;
+    // Getters - all marked const as they don't modify state
+    CardType getType() const { return type; }
+    int getValue() const { return value; }
 
-    // methods to be implemented in the card classes
+    // Card abilities - all marked const except play which modifies game state
+    virtual void executeAbility(Game& game, Player& player) = 0;
+    virtual std::string getAbilityDescription() const = 0;
+
+    // Utility methods - all marked const as they don't modify state
     virtual int getScoreValue() const;
     virtual std::string str() const = 0;
-    virtual void play(Game& game, Player& player) = 0;
-    virtual void willAddToBank(Game& game, Player& player);
+
+    // Game mechanics
+    virtual void play(Game& game, Player& player);
+    virtual void willAddToBank(Game& game, Player& player) const;
+
+    // Check if this card would cause a bust in the given play area
+    bool wouldCauseBust(const std::vector<CardPtr>& playArea) const;
+
+    // Check if this card can be played on top of another card
+    virtual bool canPlayOn(const CardPtr& other) const = 0;
 };
